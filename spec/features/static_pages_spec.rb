5a7dd673b9ff3a0fdd312118a5b4468711194b42
Drew Moxon <drew.moxon@gmail.com>
2014-01-19 00:18:05 -0800

    5.3 name routes, link to routes instead of stubs, simplify rspec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,76 +5,37 @@ feature "Static pages" do
 
  	# Define base_title for all contexts and scenarios to reference (DRY) 
  	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+ 	subject { page }
 
 	context "Home page" do
+		before { visit root_path }
 
-	 	scenario "should have the content 'Sample App'" do
-	  		visit '/static_pages/home'
-	  		expect(page).to have_content('Sample App')
-		end
+	 	it { should have_content('Sample App') }
+	 	it { should have_title(full_title('')) }
+	 	it { should_not have_title('Home') }
 
-		scenario "should have the base title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("#{base_title}")
-		end
-
-		scenario "should not have a custom page title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('Home')
-		end
 	end
 
 	context "Help page" do
+		before { visit help_path }
 
-		scenario "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+		it { should have_content('Help') }
+	 	it { should have_title(full_title('Help')) }
 
-		scenario "should have a base title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("#{base_title}")
-		end
-
-		scenario "should not have a custom page title" do
-			visit '/static_pages/help'
-			expect(page).not_to have_title('Help')
-		end
 	end
 
 	context "About page" do
+		before { visit about_path }
 
-		scenario "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
-
-		scenario "should have the base title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("#{base_title}")
-		end
-
-		scenario "should not have a custom page title" do
-			visit '/static_pages/about'
-			expect(page).not_to have_title('About Us')
-		end
+		it { should have_content('About Us') }
+		it { should have_title(full_title('About')) }
 	end
 
 	context "Contact page" do
+		before { visit contact_path }
 
-		scenario "should have the content 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact')
-		end
+		it { should have_content('Contact') }
+	 	it { should have_title(full_title('Contact')) }
 
-		scenario "should have the base title" do
-			visit '/static_pages/contact'
-			expect(page).to have_title("#{base_title}")
-		end
-
-		scenario "should not have a custom page title" do
-			visit '/static_pages/contact'
-			expect(page).not_to have_title('Contact')
-		end
 	end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# This is gross duplication of /app/helpers/application_helper. DRY this up.
+def full_title(page_title)
+	base_title = "Ruby on Rails Tutorial Sample App"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
